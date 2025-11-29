601bf1d47bf638169bf6384ac18efa9398c923ce
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2025-11-29 10:05:08 -0500

    switch to rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,12 +1,10 @@
-import { lezer } from "@lezer/generator/rollup";
-import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rolldown";
 
-export default {
+export default defineConfig({
   input: "src/index.ts",
   external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: [
     { file: "dist/index.cjs", format: "cjs" },
     { dir: "./dist", format: "es" },
   ],
-  plugins: [lezer(), typescript()],
-};
+});
